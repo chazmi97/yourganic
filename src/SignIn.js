e41ecd37440398f9fd159b4db2d919ff80782f7d
adamalfirdauz <adam_22@apps.ipb.ac.id>
2018-04-22 20:32:54 +0700

    login not fix

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { View, Text, StyleSheet, ImageBackground, Image, Platform} from 'react-native';
+import { 
+    View, Text, StyleSheet, ImageBackground, Image, Platform,
+    KeyboardAvoidingView
+} from 'react-native';
 import { 
     Container, Header, Left, Body, Right, Button, Icon, 
     Title, Content, FooterTab, Footer, Form, Item, Input ,
@@ -12,45 +15,44 @@ var logo = require('../assets/image/Logo.png');
 class SignIn extends React.Component{
     render() {
         return (
-            <Container style={styles.headerStyle}>
-                <ImageBackground source={myBackground}
-                    style={{ width: '100%', height: '100%' }}>
-                    <Header style={{ backgroundColor: 'transparent', marginTop: 18 }} noShadow>
-                        <Left>
-                            <Button transparent>
-                                <Icon name='arrow-back' />
-                            </Button>
-                        </Left>
-                        <Body>
-                            <Title>Login</Title>
-                        </Body>
-                        <Right />
-                    </Header>
-                    <ScrollView style={{flex: 1}}>
-                        {/* <View style={styles.container}> */}
-                        <Image source={logo} style={styles.logo} />
-                        <Text style={styles.titleStyle}>Welcome to Yourganic!</Text>
-                        {/* </View> */}
-                        {/* <Container> */}
-                        <Form>
-                            <Item floatingLabel>
-                                <Label style={styles.labelStyle}>Username</Label>
-                                <Input style={styles.inputTextStyle} />
-                            </Item>
-                            <Item floatingLabel last>
-                                <Label style={styles.labelStyle}>Password</Label>
-                                <Input style={styles.inputTextStyle} />
-                            </Item>
-                        </Form>
-                        <Button
-                            block={true}
-                            style={styles.buttonStyle}>
-                            <Text style={styles.buttonTextStyle}>Sign In</Text>
-                        </Button>
-                        {/* </Container> */}
-                    </ScrollView>
-                </ImageBackground>
-            </Container>
+            <KeyboardAvoidingView behavior="padding">
+                {/* <Container> */}
+                    <ImageBackground source={myBackground}
+                        style={{ width: '100%', height: '100%' }}>
+                        <ScrollView style={{ flex: 1 }}>
+                            <Header style={styles.headerStyle} noShadow>
+                                <Left>
+                                    <Button transparent>
+                                        <Icon name='arrow-back' />
+                                    </Button>
+                                </Left>
+                                <Body>
+                                    <Title>Login</Title>
+                                </Body>
+                                <Right />
+                            </Header>
+                            <Image source={logo} style={styles.logo} />
+                            <Text style={styles.titleStyle}>Welcome to Yourganic!</Text>
+                            <Form style={{ justifyContent: 'flex-end', flex: 3 }}>
+                                <Item floatingLabel>
+                                    <Label style={styles.labelStyle}>Username</Label>
+                                    <Input style={styles.inputTextStyle} />
+                                </Item>
+                                <Item floatingLabel last>
+                                    <Label style={styles.labelStyle}>Password</Label>
+                                    <Input style={styles.inputTextStyle} />
+                                </Item>
+                                <Button
+                                    block={true}
+                                    style={styles.buttonStyle}>
+                                    <Text style={styles.buttonTextStyle}>Sign In</Text>
+                                </Button>
+                            </Form>
+                            <Text style={{ color: 'white', alignSelf: 'center', marginTop: 30, flex: 1, flexDirection: 'row', alignContent: 'flex-end' }}> Dont have account? SIGN UP</Text>
+                        </ScrollView>
+                    </ImageBackground>
+                {/* </Container> */}
+            </KeyboardAvoidingView> 
         );
     }
 }
@@ -63,7 +65,8 @@ const styles = StyleSheet.create({
     titleStyle: {
         fontSize: 25,
         color: 'white',
-        alignItems: 'center'
+        // alignItems: 'center',
+        alignSelf: 'center'
     },
     buttonStyle: {
         marginTop: 10,
@@ -73,19 +76,21 @@ const styles = StyleSheet.create({
         color: 'white'
     },
     logo: {
-        flex: 1,
-        alignItems: 'center',
+        // flex: 1,
         justifyContent: 'center',
         marginTop: 25,
         marginBottom: 20,
-        width: 100,
-        height: 100,
-        resizeMode: 'contain'
+        width: 150,
+        height: 150,
+        resizeMode: 'contain',
+        alignSelf: 'center',
     },
     headerStyle: {
+        backgroundColor: 'transparent', 
+        marginTop: Platform.OS === "android" ? 13 : 0,
         // paddingTop: 18,
         // marginTop: Platform.OS === "android" ? 18 : 0,
-        backgroundColor: 'gray',
+        // backgroundColor: 'gray',
     },
     labelStyle: {
         color: 'white',
