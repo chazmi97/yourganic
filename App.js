26705a9790923450adca85bb77fba62f54b90786
Ryan Azrian <azrian.ryan@gmail.com>
2018-04-22 18:01:51 +0700

    Ryan Register PAge

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SignIn from './src/SignIn';
+import Register from './src/Register';
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <SignIn />
+        <Register />
       </View>
     );
   }
---
+++ src/Register.js
@@ -0,0 +1,133 @@
+import React from 'react'
+import {View,Text,StyleSheet,ImageBackground, TextInput, ScrollView} from 'react-native';
+// import { TextField } from 'react-native-material-textfield';
+import { 
+    Container, Header, Left, Body, Right, Button, Icon, 
+    Title, Content, FooterTab, Footer, Form, Item, Input, Label
+} from 'native-base';
+import {StackNavigator} from 'react-navigation';
+
+import SignIn from './SignIn';
+// import Register from './register';
+
+  
+
+const App = StackNavigator({
+    SignIn: { screen: SignIn },
+    // Register: { screen: Register },
+  });
+
+var myBackground = require('../assets/image/pink.jpg');
+
+class Register extends React.Component{
+    constructor(props) {
+        super(props);
+      }
+      state = {
+        email: '',
+        password:'',
+        retype :'',
+        phone:''
+      };
+
+      redirect(){
+          this.props.navigation.navigate('SignIn')
+      }
+    render() {
+        // const { navigate } = this.props.navigation;
+        return (
+            <Container style={styles.headerStyle}>   
+                <ImageBackground source={myBackground}
+                    style={{ width: '100%', height: '100%' }}>
+                    <Header style={{ backgroundColor: 'transparent' }} noShadow>
+                        <Left>
+                            <Button transparent         
+                                title="Go to Jane's profile"
+                                onPress={() =>
+                                this.redirect()}>
+                                <Icon name='arrow-back' />
+                            </Button>
+                        </Left>
+                        <Body>
+                            <Title style={styles.title}>Register</Title>
+                        </Body>
+                        <Right />
+                    </Header>
+                    <ScrollView style={styles.container}>
+                        <Text style={styles.titleStyle}>
+                            Welcome to Yourganic!
+                        </Text>
+                        <Form>
+                            <Item floatingLabel>
+                                 <Label style={styles.title}>Email</Label>
+                                 <Input style={styles.input} 
+                                 onChangeText={(email) => this.setState({email})} 
+                                 />
+                            </Item>
+                            <Item floatingLabel >
+                                <Label style={styles.title}>Password</Label>
+                                <Input 
+                                style={styles.input}
+                                secureTextEntry= {true} 
+                                onChangeText={(password) => this.setState({password})}
+                                />
+                            </Item>
+
+                            <Item floatingLabel >
+                                <Label style={styles.title}>Retype Password</Label>
+                                <Input 
+                                style={styles.input}
+                                secureTextEntry= {true} 
+                                onChangeText={(retype) => this.setState({retype})}
+                            />
+                            </Item>
+
+                            <Item floatingLabel >
+                                <Label style={styles.title}>Handphone</Label>
+                                <Input 
+                                style={styles.input}
+                                keyboardType={"numeric"}
+                                onChangeText={(handphone) => this.setState({handphone})}
+                                />
+                            </Item>
+                        </Form>
+
+                        <Button
+                            block={true}
+                            style={styles.buttonStyle}>
+                            <Text style={styles.buttonTextStyle}>Register</Text>
+                        </Button>
+                    </ScrollView>
+                </ImageBackground>
+            </Container>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // justifyContent: 'center', 
+    },
+    titleStyle: {
+        fontSize: 30,
+        color: 'white',
+        alignSelf: 'center',
+        margin:20,
+        paddingTop: 10,
+    },
+    buttonStyle: {
+        margin: 10
+    },
+    buttonTextStyle: {
+        color: 'white',
+
+    },
+    title:{
+        color: 'white'
+    },
+    input:{
+        color: 'white',
+    }
+});
+
+export default Register;
