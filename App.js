c6b0089946e5ccd335ccf2563ed265e2712f788c
adamalfirdauz <adam_22@apps.ipb.ac.id>
2018-04-22 18:04:50 +0700

    login not fix

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SignIn from './src/SignIn';
+import { Font, AppLoading } from "expo";
+
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ fontLoaded: true });
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <SignIn />
-      </View>
-    );
+    if(this.state.fontLoaded){
+      return (
+        <View style={styles.container}>
+          <SignIn />
+        </View>
+      );
+    }
+    else {
+      return (
+        <Text></Text>
+      );
+    }
   }
 }
 
